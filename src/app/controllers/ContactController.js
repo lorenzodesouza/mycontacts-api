ed97a794a95200afada55a00f735b192a1d8208c
Lorenzo <Lorenzocontato01@gmail.com>
2023-02-19 01:54:44 -0300

    feat: criando rotas de edicao de contatos

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -32,9 +32,8 @@ class ContactController {
     }
 
     const contactExists = await ContactsRepository.findByEmail(email);
-
     if (contactExists) {
-      return response.status(400).json({ erro: 'this email is already been taken'});
+      return response.status(400).json({ erro: 'this email is already is use'});
     }
 
     const contact = await ContactsRepository.create({
@@ -44,8 +43,32 @@ class ContactController {
     response.json(contact);
   }
 
-  update() {
+  async update(request, response) {
     // Editar um registro
+    const { id } = request.params
+    const { name, email, phone, category_id } = request.body
+
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      // 404: not found (usuario nao encontrado)
+      return response.status(404).json({ erro: 'User not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ erro: 'name is required'});
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ erro: 'this email is already is use'});
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id
+    });
+
+    response.json(contact);
   }
 
   async delete(request, response) {
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -52,6 +52,22 @@ class ContactsRepository {
       resolve(newContact);
     });
   }
+  update(id, { name, email, phone, category_id }) {
+    return new Promise((resolve) => {
+      const updateContact = {
+        id,
+        name,
+        email,
+        phone,
+        category_id,
+      };
+      contacts = contacts.map((contact) => (
+        contact.id === id ? updateContact : contact
+      ));
+
+      resolve(updateContact);
+    });
+  }
 }
 
 module.exports = new ContactsRepository();
