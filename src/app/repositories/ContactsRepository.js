000cf60bab848871cd69384303d7f4fc75afab71
Lorenzo <Lorenzocontato01@gmail.com>
2023-02-20 20:30:43 -0300

    feat: postgresql, config do postgres / refatorado create

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,5 +1,7 @@
 const { v4 } = require('uuid');
 
+const db = require('../../database/index');
+
 let contacts = [
   {
     id: v4(),// 'uuid -> Universal Unique ID'
@@ -39,34 +41,14 @@ class ContactsRepository {
       resolve();
     });
   }
-  create({ name, email, phone, category_id }) {
-    return new Promise((resolve) => {
-      const newContact = {
-        id: v4(),
-        name,
-        email,
-        phone,
-        category_id,
-      };
-      contacts.push(newContact);
-      resolve(newContact);
-    });
-  }
-  update(id, { name, email, phone, category_id }) {
-    return new Promise((resolve) => {
-      const updateContact = {
-        id,
-        name,
-        email,
-        phone,
-        category_id,
-      };
-      contacts = contacts.map((contact) => (
-        contact.id === id ? updateContact : contact
-      ));
+  async create({ name, email, phone, category_id }) {
+    const [row] = await db.query(`
+    INSERT INTO contacts(name, email, phone, category_id)
+    VALUES($1, $2, $3, $4)
+    RETURNING *
+    `, [name, email, phone, category_id]);
 
-      resolve(updateContact);
-    });
+    return row;
   }
 }
 
