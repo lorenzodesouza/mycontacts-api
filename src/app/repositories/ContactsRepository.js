ebdda2a00628a047ed87273d128639e820f935f8
Lorenzo <Lorenzocontato01@gmail.com>
2023-02-20 21:48:28 -0300

    feat: refatorando o método update

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -33,12 +33,6 @@ class ContactsRepository {
     const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email]);
     return row;
   }
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
-  }
   async create({ name, email, phone, category_id }) {
     const [row] = await db.query(`
     INSERT INTO contacts(name, email, phone, category_id)
@@ -47,6 +41,21 @@ class ContactsRepository {
     `, [name, email, phone, category_id]);
 
     return row;
+  }
+  async update(id, { name, email, phone, category_id }) {
+    const [row] = await db.query(`
+      UPDATE contacts
+      SET name = $1, email = $2, phone = $3, category_id = $4
+      WHERE id = $5
+      RETURNING *
+    `, [name, email, phone, category_id, id]);
+    return row;
+  }
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
   }
 }
 
